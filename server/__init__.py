045b6b985a37e42fa7baf50482e49e528b875e5a
Roy Nieterau <roy_nieterau@hotmail.com>
2024-11-07 14:29:53 +0100

    Remove broken server version reference

--- server/__init__.py
+++ server/__init__.py
@@ -2,14 +2,12 @@ from typing import Type
 
 from ayon_server.addons import BaseServerAddon
 
-from .version import __version__
 from .settings import LaunchScriptsSettings, DEFAULT_VALUES
 
 
 class LaunchScriptsAddon(BaseServerAddon):
     name = "launch_scripts"
     title = "Launch Scripts"
-    version = __version__
     settings_model: Type[LaunchScriptsSettings] = LaunchScriptsSettings
 
     async def get_default_settings(self):
